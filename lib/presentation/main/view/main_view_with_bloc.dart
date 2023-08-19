04650b6451cc88d395e043c762fa303246b90df1
Hamada-Saad-geeksjo <hamada.saad@geeksjo.com>
2023-08-19 17:48:11 +0300

    update project

--- lib/presentation/main/view/main_view_with_bloc.dart
+++ lib/presentation/main/view/main_view_with_bloc.dart
@@ -79,6 +79,7 @@ class _MainViewCubitState extends State<MainViewCubit>
                 )
               ],
             ),
+
       bottomSheet: !isDataLoaded
           ? const SizedBox()
           : SizedBox(
