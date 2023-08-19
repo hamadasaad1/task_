49e8dc0eb5d486001be0b28a85b3e4dc67bed886
Hamada-Saad-geeksjo <hamada.saad@geeksjo.com>
2023-08-20 02:04:29 +0300

    update project

--- scripts/beta-android.sh
+++ scripts/beta-android.sh
@@ -1,4 +1,17 @@
 
+# cd android
+# bundle exec fastlane beta_android
+# cd ..
+
+
 cd android
+chmod +x gradlew
+./gradlew clean
+bundle install
 bundle exec fastlane beta_android
 cd ..
+
+
+
+
+
--- scripts/prepare-android.sh
+++ scripts/prepare-android.sh
@@ -1,5 +1,6 @@
 # Clean Android project and setup
 cd android
+chmod +x gradlew
 ./gradlew clean
 bundle install
 bundle exec fastlane install_plugins
